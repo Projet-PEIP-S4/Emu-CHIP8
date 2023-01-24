ec95bcb2c9be711476703410110ccf0186acfd08
valentin-llv <lelievre.valentin17@gmail.com>
2023-01-24 09:27:30 +0100

    Added type control and updated README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Emu-CHIP8
+
+## Requierements
+
+Use python Mypy for types checking
+
+```properties
+pip install mypy
+```
+
+## Run
+
+Run the file if type check exit succefully.
+
+```properties
+cmd /c "mypy main.py && python main.py"
+```
--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from utils.localDataManager import getGames, getGameFile
 
+logging = True
+def log(log: str) -> None:
+    if logging:
+        print(log)
+
+class Mem:
+    instance = None
+
+    @staticmethod
+    def getInstance():
+        if Mem.instance == None: Mem.instance = Mem()
+        return Mem.instance
+
+    def __init__(self):
+        log("New memory instance")
+        self.test = "bnojour"
+
 def main():
     print(getGames())
 
+    Mem.getInstance()
     fileData = getGameFile("MISSILE")
 
 main()
