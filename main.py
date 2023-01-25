1bc4b4c256b325cb9784943c81c05c3e3967abc8
LANGLAIS Amandine <amandine.langlai03s@gmail.com>
2023-01-25 15:58:46 +0100

    Creation class CPU and Edit of loop()

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class Mem:
     instance: object | None = None
 
     @staticmethod
-    def getInstance() -> object:
+    def getInstanceMem() -> object:
         if Mem.instance == None: Mem.instance = Mem()
         return Mem.instance
 
@@ -49,16 +49,45 @@ class Mem:
 
         return allVarsFormated
 
+class CPU:
+    instance: object | None = None
+    #print(CPU) à configurer 
+    @staticmethod
+    def getInstanceCPU() -> object:
+        if CPU.instance == None: CPU.instance = CPU()
+        return CPU.instance
+
+    def __init__(self) -> None:
+        self.reset()
+
+    def reset(self) -> None:
+        self.code = 0
+        self.vx = 0
+        self.vy = 0
+        self.n = 0
+    
+    def __str__(self) -> str:
+        return f"code : {self.code}, vx : {self.vx}, vy : {self.vy}, n : {self.n}"
+
+    def decode(self,instruction : int ):
+        self.code = (instruction & 0xf000) >> 12
+        self.vx = (instruction & 0x0f00) >> 8
+        self.vy = (instruction & 0x00f0) >> 4
+        self.n = (instruction & 0x000f) 
+        
 def loop():
     gameOn = True
     while gameOn: 
         #récupère l'instruction à effectuer
-        pc = Mem.getInstance().pc
-        instructionArray = Mem.getInstance().mem[pc : pc + 16]
-        instruction =  Utils.convertBinArrayToHexStr(instructionArray[0:8]) + Utils.convertBinArrayToHexStr(instructionArray[8:16]) 
+        pc = Mem.getInstanceMem().pc
+        instructionArray = Mem.getInstanceMem().mem[pc : pc + 16]
+        instruction =  Utils.convertBinArrayToHexStr(instructionArray[0:8])[2:] + Utils.convertBinArrayToHexStr(instructionArray[8:16])[2:]
         print(instruction)
-        gameOn = False
         #execution la commande
+        CPU.getInstanceCPU().decode(int(instruction))
+        gameOn = False
+        print(CPU.getInstanceCPU())
+        
         #incrémente le pc
 
 
@@ -67,7 +96,7 @@ def loop():
 def main():
     fileData = getGameFile("MISSILE")
     
-    Mem.getInstance().fillMemory(fileData)
+    Mem.getInstanceMem().fillMemory(fileData)
     loop()
 
 main()
